c843dc29d00d616245bfaecf2efc8768f8401aeb
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-10-28 23:06:10 +0000

    initial commit

---
+++ Module9Challenge/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace VideoGames
+{
+    public class VideoGames
+    {
+        public required string Name { get; set; }
+        public required string Genre { get; set; }
+        public double Rating { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //  Create a list of VideoGame objects
+            List<VideoGame> games = new List<VideoGame>
+            {
+                new VideoGame { Name = "Sonic", Genre = "Platform", Rating = 4.3 },
+                new VideoGame { Name = "Tetris", Genre = "Puzzle", Rating = 3.9 },
+                new VideoGame { Name = "Sudoku", Genre = "Puzzle", Rating = 4.0 },
+                new VideoGame { Name = "Snake", Genre = "Arcade", Rating = 4.1 },
+                new VideoGame { Name = "Mario", Genre = "Platform", Rating = 4.5 }
+            };
+
+            //  Filter games with a rating of 4 or higher
+            var topRatedGames = (from game in games
+                                 where game.Rating >= 4
+                                 select game).ToList();
+
+            //  Sort the filtered list by game name
+            var sortedGames = (from game in topRatedGames
+                               orderby game.Name
+                               select game).ToList();
+
+            // Print out the sorted list of game names
+            foreach (var game in sortedGames)
+            {
+                Console.WriteLine(game.Name);
+            }
+        }
+    }
+}
---
+++ Module9Challenge/Videogames.cs
@@ -0,0 +1,10 @@
+namespace VideoGames
+{
+    public class VideoGame
+    {
+        //Attributes (properties)
+        public required String Name { get; set; }
+        public required String Genre { get; set; }
+        public Double Rating { get; set; }
+    }
+}
